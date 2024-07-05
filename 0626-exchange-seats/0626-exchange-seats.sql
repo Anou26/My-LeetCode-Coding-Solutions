073bb9fdf344cc8ca9b3585a1e69e07d2f2587d1
AnouMer <47783955+Anou26@users.noreply.github.com>
2024-07-05 18:50:56 -0400

    Time: 307 ms (85.76%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,12 @@
+SELECT 
+    CASE
+        WHEN MOD(s1.id, 2) = 1 AND s2.id IS NOT NULL THEN s2.id
+        WHEN MOD(s1.id, 2) = 0 THEN s1.id - 1
+        ELSE s1.id
+    END AS id,
+    s1.student
+FROM 
+    Seat s1
+LEFT JOIN 
+    Seat s2 ON s1.id = s2.id - 1
+ORDER BY id;
