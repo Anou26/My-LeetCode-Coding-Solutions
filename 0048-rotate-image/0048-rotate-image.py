938f58fd1c745e1bbd5a7789d92d737fe5884127
AnouMer <47783955+Anou26@users.noreply.github.com>
2024-07-10 18:13:28 -0400

    Time: 23 ms (20.14%), Space: 11.6 MB (32.28%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+
+        # Transpose the matrix
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # Reverse each row
+        for i in range(n):
+            matrix[i].reverse()
+
+# Example usage:
+sol = Solution()
+matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+sol.rotate(matrix1)
+print(matrix1)  # Output: [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
+matrix2 = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+sol.rotate(matrix2)
+print(matrix2)  # Output: [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
