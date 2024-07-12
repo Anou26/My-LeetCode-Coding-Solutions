887e21294fec092d37b7c4920bf564bd6ff8de06
AnouMer <47783955+Anou26@users.noreply.github.com>
2024-07-12 14:13:19 -0400

    Time: 280 ms (59.47%), Space: 14.1 MB (84.27%) - LeetHub

---
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def removeStars(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []
+        
+        for char in s:
+            if char == '*':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(char)
+        
+        return ''.join(stack)
+
+# Example usage:
+sol = Solution()
+print(sol.removeStars("leet**cod*e"))  # Output: "lecoe"
+print(sol.removeStars("erase*****"))  # Output: ""
